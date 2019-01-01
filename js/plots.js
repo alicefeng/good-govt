e4b51f00042c5be22a0f2e7677154ca766fd51b4
Alice Feng <feng.a.g@gmail.com>
2018-12-31 22:36:37 -0500

    Add in beeswarms for metrics from Heritage

--- js/plots.js
+++ js/plots.js
@@ -1,6 +1,6 @@
 var margin = {top: 20, right: 20, bottom: 20, left: 40},
-	width = 500,
-	height = 500;
+	width = 450,
+	height = 450;
 
 var edScale = d3.scaleLinear().range([0, width]);
 var healthScale = d3.scaleLinear().range([height, 0]);
@@ -10,11 +10,14 @@ var beeMargin = {top: 20, right: 75, bottom: 20, left: 75},
 	beeHeight = 500;
 
 var systemMetricsScale = d3.scalePoint()
-	.domain(["Political stability", "Government effectiveness", "Regulatory quality", "Rule of law", "Control of corruption"])//, "judicial_effectiveness_score", "government_integrity_score", "property_rights_score", "overall_economic_freedom_score", "financial_freedom_score"])
 	.range([0, beeWidth]);
-var wbScale = d3.scaleLinear().domain([-3, 2.5]).range([height, 0]);
+var wbScale = d3.scaleLinear().domain([-3, 2.5]).range([beeHeight, 0]);
+var heritageScale = d3.scaleLinear().domain([0, 100]).range([beeHeight, 0]);
 
 var colorScale = d3.scaleOrdinal().domain([0, 1, 2]).range(["#d2d2d2", "#d2d2d2", "#ffe5cc"]);
+
+var wbVars = ["Political stability", "Government effectiveness", "Regulatory quality", "Rule of law", "Control of corruption"];
+var heritageVars = ["Judicial effectiveness", "Government integrity", "Property rights", "Overall economic freedom", "Financial freedom"];
 
 d3.csv("data/data.csv", function(d) {
 	return {
@@ -38,10 +41,28 @@ d3.csv("data/data.csv", function(d) {
 	if(error) throw error;
 
 	// console.log(data);
+	var metricsData = [];
+	data.forEach(function(d) {
+		(!isNaN(d.political_stability)) && metricsData.push({'country_name': d.country_name, 'metric': 'Political stability', 'score': d.political_stability, 'top_country': d.top_country});
+		(!isNaN(d.gov_effectiveness)) && metricsData.push({'country_name': d.country_name, 'metric': 'Government effectiveness', 'score': d.gov_effectiveness, 'top_country': d.top_country});
+		(!isNaN(d.regulatory_quality)) && metricsData.push({'country_name': d.country_name, 'metric': 'Regulatory quality', 'score': d.regulatory_quality, 'top_country': d.top_country});
+		(!isNaN(d.rule_of_law)) && metricsData.push({'country_name': d.country_name, 'metric': 'Rule of law', 'score': d.rule_of_law, 'top_country': d.top_country});
+		(!isNaN(d.control_corruption)) && metricsData.push({'country_name': d.country_name, 'metric': 'Control of corruption', 'score': d.control_corruption, 'top_country': d.top_country});
+		(!isNaN(d.judicial_effectiveness_score)) && metricsData.push({'country_name': d.country_name, 'metric': 'Judicial effectiveness', 'score': d.judicial_effectiveness_score, 'top_country': d.top_country});
+		(!isNaN(d.government_integrity_score)) && metricsData.push({'country_name': d.country_name, 'metric': 'Government integrity', 'score': d.government_integrity_score, 'top_country': d.top_country});
+		(!isNaN(d.property_rights_score)) && metricsData.push({'country_name': d.country_name, 'metric': 'Property rights', 'score': d.property_rights_score, 'top_country': d.top_country});
+		(!isNaN(d.overall_economic_freedom_score)) && metricsData.push({'country_name': d.country_name, 'metric': 'Overall economic freedom', 'score': d.overall_economic_freedom_score, 'top_country': d.top_country});
+		(!isNaN(d.financial_freedom_score)) && metricsData.push({'country_name': d.country_name, 'metric': 'Financial freedom', 'score': d.financial_freedom_score, 'top_country': d.top_country});
+	});
+	// console.log(metricsData);
+
+
 	edScale.domain([0, d3.max(data, function(d) { return d.edu_expenditure_per_person; })]).nice();
 	healthScale.domain([0, d3.max(data, function(d) { return d.health_expenditure_per_person; })]).nice();
+
 	makeScatterPlot(data);
-	makeBeeswarm(data);
+	makeBeeswarm(metricsData.filter(function(d) { return wbVars.indexOf(d.metric) > -1; }), "#stable_system1", wbVars, wbScale);
+	makeBeeswarm(metricsData.filter(function(d) { return heritageVars.indexOf(d.metric) > -1; }), "#stable_system2", heritageVars, heritageScale);
 });
 
 function makeScatterPlot(data) {
@@ -88,18 +109,10 @@ function makeScatterPlot(data) {
 		.style("fill", function(d) { return colorScale(d.top_country); });
 }
 
-function makeBeeswarm(data) {
-	var wbData = [];
-	data.forEach(function(d) {
-		(!isNaN(d.political_stability)) && wbData.push({'country_name': d.country_name, 'metric': 'Political stability', 'score': d.political_stability, 'top_country': d.top_country});
-		(!isNaN(d.gov_effectiveness)) && wbData.push({'country_name': d.country_name, 'metric': 'Government effectiveness', 'score': d.gov_effectiveness, 'top_country': d.top_country});
-		(!isNaN(d.regulatory_quality)) && wbData.push({'country_name': d.country_name, 'metric': 'Regulatory quality', 'score': d.regulatory_quality, 'top_country': d.top_country});
-		(!isNaN(d.rule_of_law)) && wbData.push({'country_name': d.country_name, 'metric': 'Rule of law', 'score': d.rule_of_law, 'top_country': d.top_country});
-		(!isNaN(d.control_corruption)) && wbData.push({'country_name': d.country_name, 'metric': 'Control of corruption', 'score': d.control_corruption, 'top_country': d.top_country});
-	});
-	// console.log(wbData);
+function makeBeeswarm(data, divId, vars, xScale) {
+	systemMetricsScale.domain(vars);
 
-	var svg = d3.select("#stable_system")
+	var svg = d3.select(divId)
 		.append("svg")
 		.attr("width", beeWidth + beeMargin["left"] + beeMargin["right"])
 		.attr("height", beeHeight + beeMargin["top"] + beeMargin["bottom"])
@@ -131,16 +144,16 @@ function makeBeeswarm(data) {
 		.attr("fill", "#000")
 		.text("Worse");
 
-	var simulation = d3.forceSimulation(wbData)
+	var simulation = d3.forceSimulation(data)
 		.force("x", d3.forceX(function(d) { return systemMetricsScale(d.metric); }))
-		.force("y", d3.forceY(function(d) { return wbScale(d.score); }).strength(1))
+		.force("y", d3.forceY(function(d) { return xScale(d.score); }).strength(1))
 		.force("collide", d3.forceCollide(4))
 		.stop();
 
-	for(var i = 0; i < data.length; ++i) simulation.tick();
+	for(var i = 0; i < 20; ++i) simulation.tick();
 
 	var countries = svg.selectAll(".country")
-		.data(wbData)
+		.data(data)
 		.enter()
 		.append("circle")
 		.attr("class", "country")
